618e251ae7e6f1c4d878c56af1c529ad65f0e56a
Yacine Saoudi <yacine@yacinesmbp73662.home>
2024-05-22 17:33:06 -0400

    write VGA buffer additions
    add color enum for text, created writer that handles writing valid text to buffer, add write_str required function for fmt::write, created generic print_something function to test out printing.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,21 @@
 
 use core::panic::PanicInfo;
 
+use vga_buffer::print_something;
+
+static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xcf;
+        }
+    }
+    print_something();
     loop{}
 }
 
